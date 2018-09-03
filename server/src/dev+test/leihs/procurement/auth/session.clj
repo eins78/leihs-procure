2cc5b7088fc2f8d3fd939b0b4e77640326a5449b
Max Albrecht <max.albrecht@zhdk.ch>
2018-09-03 17:58:14 +0200

    server: add fake-cookie-auth to dev/test sessions

--- server/src/dev+test/leihs/procurement/auth/session.clj
+++ server/src/dev+test/leihs/procurement/auth/session.clj
@@ -1,12 +1,23 @@
 (ns leihs.procurement.auth.session
-  (:require [leihs.procurement.resources.user :as u]))
+  (:require [leihs.procurement.resources.user :as u]
+            [clojure.tools.logging :as log]))
 
 (defn wrap
   [handler]
   (fn [request]
-    (let [user-id (some-> request
-                          :headers
-                          (get "x-fake-token-authorization")
-                          (->> (u/get-user-by-id (:tx request)))
-                          :id)]
-      (handler (assoc request :authenticated-entity {:user_id user-id})))))
+    (let [user-id-from-cookie (-> request
+                                  :cookies
+                                  (get "leihs-fake-cookie-auth" nil)
+                                  :value)
+          user-id-from-token (some-> request
+                                     :headers
+                                     (get "leihs-fake-token-auth")
+                                     (->> (u/get-user-by-id (:tx request)))
+                                     :id)]
+      (if user-id-from-cookie
+        (log/debug "[>- leihs-fake-cookie-auth]" user-id-from-cookie))
+      (if user-id-from-token
+        (log/debug "[>- leihs-fake-token-auth]" user-id-from-token))
+      (handler (assoc request
+                 :authenticated-entity {:user_id (or user-id-from-cookie
+                                                     user-id-from-token)})))))
