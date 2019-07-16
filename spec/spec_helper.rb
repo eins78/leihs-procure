bb054834eef5edf018935db570c01515cc4875b1
Matus Kmit <matus.kmit@zhdk.ch>
2019-07-16 12:01:54 +0200

    wip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'active_support/all'
 require 'pry'
 
 require 'config/database'
-require 'config/emails'
 require 'config/factories'
 require 'config/metadata_extractor'
 require 'config/screenshots'
@@ -11,8 +10,8 @@ require 'config/screenshots'
 ACCEPTED_FIREFOX_ENV_PATHS = ['FIREFOX_ESR_60_PATH']
 
 # switch to HTTPS ?
-LEIHS_HTTP_BASE_URL = ENV['LEIHS_HTTP_BASE_URL'].presence || 'http://localhost:3200'
-LEIHS_HTTP_PORT =  Addressable::URI.parse(LEIHS_HTTP_BASE_URL).port.presence  || '3200'
+LEIHS_HTTP_BASE_URL = ENV['LEIHS_HTTP_BASE_URL'].presence || 'http://localhost:3230'
+LEIHS_HTTP_PORT =  Addressable::URI.parse(LEIHS_HTTP_BASE_URL).port.presence  || '3230'
 
 BROWSER_WINDOW_SIZE = [ 1200, 800 ]
 
